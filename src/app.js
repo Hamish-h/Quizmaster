1281a49625ab5842b9d4d83634c15340ea5d1750
Hamish-H <hamish.h@outlook.com>
2018-06-30 22:39:44 +0100

    updated app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+const Quizmaster = require('./models/quizmaster.js');
+const QuizmasterView = require('./views/quizmaster_view.js');
+const QuizmasterInfo = require('./views/quizmaster_info.js');
+
 document.addEventListener('DOMContentLoaded', () => {
+
   console.log('JavaScript Loaded');
+  const quizmaster = new Quizmaster();
+  quizMaster.getData();
+
+  const quizmasterContainer = document.querySelector('#???????????');
+  const quizmasterView = new QuizmasterListView(quizMasterContainer);
+  quizmasterView.bindEvents();
+
 })
--- src/models/quizmaster.js
+++ src/models/quizmaster.js
@@ -1,2 +1,35 @@
 const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
+
+const Quizmaster = function () {
+  this.text = null;
+}
+
+
+
+
+
+
+
+
+
+
+
+//
+// Quizmaster.prototype.getData = function () {
+//   // set the url
+//   const request = new Request('https://opentdb.com/api.php?amount=50&difficulty=medium&type=multiple');
+//
+//   // function is 'onComplete'
+//   request.get((quizData) => {
+//     console.log(quizData);
+//     // collect and assign string
+//     this.text = quizData.quiz;
+//     // publish it
+//     PubSub.publish('Quizmaster:quiz-loaded', this.text);
+//   });
+
+
+}
+
+module.exports = Quizmaster;
