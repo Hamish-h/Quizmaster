5963f4dfd620f990f8c8be54a8c750fb40bffdb2
Hamish-H <hamish.h@outlook.com>
2018-06-30 23:20:18 +0100

    updated view

--- src/views/quizmaster_view.js
+++ src/views/quizmaster_view.js
@@ -1,2 +1,20 @@
 const PubSub = require('../helpers/pub_sub.js');
 const RequestHelper = require('../helpers/request_helper.js');
+
+const QuizmasterView = function (container) {
+  this.container = container;
+}
+
+QuizmasterView.prototype.bindEvents = function () {
+  PubSub.subscribe('Quizmaster:quizmaster-loaded', (event) => {
+    this.render(event.detail);
+  });
+}
+
+QuizmasterView.prototype.render = function (quizmaster) {
+  const p = document.createElement('p');
+  p.textContent = quizmaster;
+  this.container.appendChild(p);
+}
+
+module.exports = QuizmasterView;
