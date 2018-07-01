7c94b3acefb90a259ef90a34d40b740b6c358815
Hamish-H <hamish.h@outlook.com>
2018-07-01 18:04:00 +0100

    updated html css

--- src/models/quizmaster.js
+++ src/models/quizmaster.js
@@ -2,25 +2,27 @@ const Request = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
 
 const QuizMaster = function () {
-  this.text = [null];
-
+  this.text = null;
 }
+
 // get data
 QuizMaster.prototype.getData = function () {
   // set the url
-  const request = new Request('https://opentdb.com/api.php?amount=50&difficulty=medium&type=multiple');
+  const request = new Request('https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple');
   // oncomplete
   request.get((data) => {
-
-    console.log("request get results ", data);
-
+  // console.log("request get results ", data);
   // collect and assign data
-  this.text = data.results;
-
-    console.log("this.text contains ", this.text);
-
+  this.category = data.results;
+  this.question = data.results;
+  this.incorrect_answers = data.results;
+  this.correct_answer = data.results;
+  // console.log("this.category", this.category);
   // publish it
-  PubSub.publish('QuizMaster:quizMaster-loaded', this.text);
+  PubSub.publish('QuizMaster:quizMaster-loaded', this.category[0].category);
+  PubSub.publish('QuizMaster:quizMaster-loaded', this.question[0].question);
+  PubSub.publish('QuizMaster:quizMaster-loaded', this.incorrect_answers[0].incorrect_answers);
+  PubSub.publish('QuizMaster:quizMaster-loaded', this.correct_answer[0].correct_answer);
 
 });
 }
